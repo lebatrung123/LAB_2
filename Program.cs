7c0ce784c14974e507d8ae5615ec2a9ebc8a6625
lebatrung <trunglehotface@gmail.com>
2024-10-03 20:40:18 +0700

    du an hoan thien

--- Program.cs
+++ Program.cs
@@ -3,7 +3,17 @@
 
     private static void Main(string[] args)
     {
-        DateTime datetime = DateTime.Now;
-        Console.WriteLine($"{datetime.Hour}{datetime.Minute}{datetime.Second}");
+        // DateTime datetime = DateTime.Now;
+        //Console.WriteLine($"{datetime.Hour}{datetime.Minute}{datetime.Second}");
+        Console.WriteLine("Nhâp chieu rong hinh chu nhat");
+        float w = float.Parse(Console.ReadLine());
+        Console.WriteLine("Nhâp chieu dai hinh chu nhat");
+        float d = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Chu vi hcn la: {0}", (w + d) * 2);
+
+        Console.WriteLine($"Dien tich hcn la: {w * d}");
+
+
     }
 }
